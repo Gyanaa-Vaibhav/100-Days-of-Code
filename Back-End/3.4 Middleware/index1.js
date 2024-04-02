1186f6aa040b29a8253750e4e542a1b1604192cb
Gyanaa_Vaibhav <gynanrudr0@gmail.com>
2024-04-02 21:05:22 +0530

    Middleware didnt understand might comback tomorrow

--- Back-End/3.4 Middleware/index1.js
+++ Back-End/3.4 Middleware/index1.js
@@ -1,24 +1,25 @@
 import express from "express";
-import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import bodyParser from "body-parser";
+
+
 
 const app = express();
-const port = 3000;
+const port = 5500;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.get("/", (req, res) => {
-res.sendFile(__dirname + "/public/index.html");
-});
-
-app.post("/submit", (req, res) => {
-  console.log(req.body);
+  res.sendFile(__dirname + "/public/index.html");
+  // console.log(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+})
